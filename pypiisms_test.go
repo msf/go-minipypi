477591a747514041f5cd275677e0cb843c50d688
Miguel Filipe <miguel.filipe@gmail.com>
2016-05-11 11:55:50 +0100

    handle pip8.1.2 DIRLIST for packages with periods in the name.
    this changes how we normalize the filenames, must test in real life.

--- pypiisms_test.go
+++ pypiisms_test.go
@@ -11,13 +11,22 @@ func TestNameNormalize(t *testing.T) {
 	}
 
 	testNames("PyCrypto", "pycrypto")
-	testNames("Django-HStore", "django_hstore")
+	testNames("Django-HStore", "django-hstore")
+	testNames("django_nose", "django-nose")
+	testNames("imposm.parser", "imposm-parser")
 }
 
 func TestPackageUrlPathToGetFilePath(t *testing.T) {
 	origPath := "/python-dateutil/python_dateutil-2.4.2-py2.py3-none-any.whl"
 	wanted := "/python_dateutil-2.4.2-py2.py3-none-any.whl"
 	path := handlePypiFileNames(origPath)
+	if path != wanted {
+		t.Errorf("failed to convert filename, wanted: %v, got: %v", wanted, path)
+	}
+
+	origPath = "/imposm-parser/imposm.parser-1.0.6-cp27-none-linux_x86_64.whl"
+	wanted = "/imposm.parser-1.0.6-cp27-none-linux_x86_64.whl"
+	path = handlePypiFileNames(origPath)
 	if path != wanted {
 		t.Errorf("failed to convert filename, wanted: %v, got: %v", wanted, path)
 	}
